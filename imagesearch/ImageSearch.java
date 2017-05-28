79d6a16e6a39f179fe38499622ce1e253a25256a
Playname <playnamegaming@gmail.com>
2017-05-28 17:12:13 +0200

    Removed unused imports

--- imagesearch/ImageSearch.java
+++ imagesearch/ImageSearch.java
@@ -3,8 +3,6 @@ package imagesearch;
 import java.io.IOException;
 import java.net.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.*;
 
 public class ImageSearch {
